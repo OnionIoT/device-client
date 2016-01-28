a8f104aff5bd2340b4e53f58b42c2a95ad047f5e
greenbreakfast <lazardemin@gmail.com>
2016-01-28 16:03:31 -0500

    inital commit: testing compile and libcurl operation

---
+++ include/device-client.h
@@ -0,0 +1,15 @@
+#ifndef _DEVICE_CLIENT_H_
+#define _DEVICE_CLIENT_H_
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+#include <onion-debug.h>
+
+#include "libubus.h"
+#include "curl/curl.h"
+
+
+#endif // _DEVICE_CLIENT_H_ 
